5667e4ac38c507f0aa49dbc26ae9355b77782105
Lukas Ingold <36853943+ilvkas@users.noreply.github.com>
2023-12-03 17:14:18 +0100

    Update Sort_Homes.py

--- Sort_Homes.py
+++ Sort_Homes.py
@@ -35,20 +35,20 @@ CubeSensorCount = 0
 
 #positions x, y, z, rx, ry, rz
 clearCamera = 0.25, -0.22, 0.20, 0, 3.14, 0
-clearCamera_2 = 0.25, -0.22, 0.20, 0, 3.14, 0
 
 #We have to change all the coordinates
 placeObjectConveyor_r1 = -0.3, 1.05, 0.15, 0, 3.14, 0
 placeObjectConveyorDown_r1 = -0.3, 1.05, 0.005, 0, 3.14, 0
-placeObjectHome_r1 = 0.3, -0.25, 0.15, 0, 3.14, 0
-placeObjectHomeDown_r1 = 0.3, -0.25, 0.005, 0, 3.14, 0
+placeObjectHome_r1 = 0.3, -0.15, 0.15, 0, 3.14, 0
+placeObjectHomeDown_r1 = 0.3, -0.15, 0.005, 0, 3.14, 0
 pickObjectConveyor_r1 = -0.3, 1.05, 0.15, 0, 3.14, 0
 pickObjectConveyorDown_r1 = -0.3, 1.05, 0.15, 0, 3.14, 0
+transitionPos_r1 = 0.0, -0.3, 0.20, 0, 3.14, 0 
 
 placeObjectConveyor_r2 = 0.3, -0.25, 0.15, 0, 3.14, 0
 placeObjectConveyorDown_r2 = 0.3, -0.25, 0.005, 0, 3.14, 0
-placeObjectHome_r2 = 0.25, -0.25, 0.20, 0, 3.14, 0
-placeObjectHomeDown_r2 = 0.25, -0.25, 0.010, 0, 3.14, 0
+placeObjectHome_r2 = 0.25, -0.15, 0.20, 0, 3.14, 0
+placeObjectHomeDown_r2 = 0.25, -0.15, 0.010, 0, 3.14, 0
 pickObjectConveyor_r2 = -0.3, 1.05, 0.15, 0, 3.14, 0
 pickObjectConveyorDown_r2 = -0.3, 1.05, 0.15, 0, 3.14, 0
 
@@ -100,8 +100,20 @@ def locateObject(object, camera1, camera2):
             time.sleep(3)
             print(x, y)
         else:
-            print("Object number",object,"not detected")
+            print("Object number ",object," not detected")
 
+def modify_object_coordinates(Object, index, gamma=None, omega=None):
+    if (gamma is None and omega is None) or (gamma is not None and omega is not None):
+        raise ValueError("Either gamma or omega should be provided, but not both")
+    adjustment_value = gamma if gamma is not None else omega
+    if index < 0 or index >= len(Object):
+        raise ValueError("Index out of range")
+    Object = list(Object)
+    Object[index] -= adjustment_value
+    Object = tuple(Object)
+    gamma = 0
+    omega = 0
+    return Object
 
 #Transition cube to home (T7)
 def CubeToHome():
@@ -112,6 +124,7 @@ def CubeToHome():
     print(pickPos)
     rob.send_program(rq_open())
     time.sleep(0.1)
+    move(rob, transitionPos_r1, True)
     move(rob, overPickPos, True)
     move(rob, pickPos, True)
     #closes gripper
@@ -124,9 +137,12 @@ def CubeToHome():
     rob.send_program(rq_open())
     time.sleep(0.6)
     move(rob, placeObjectHome_r1, True)
-    gamma += 0.075
-    placeObjectHome_r2 = 0.3, -0.25-gamma, 0.15, 0, 3.14, 0
-    placeObjectHomeDown_r2 = 0.3, -0.25-gamma, 0.005, 0, 3.14, 0
+    gamma += 0.07
+    #update value
+    placeObjectHome_r1 = modify_object_coordinates(placeObjectHome_r1, 1, gamma)
+    placeObjectHomeDown_r1 = modify_object_coordinates(placeObjectHomeDown_r1, 1, gamma)
+    #placeObjectHome_r1 = 0.3, -0.15-gamma, 0.15, 0, 3.14, 0
+    #placeObjectHomeDown_r1 = 0.3, -0.15-gamma, 0.005, 0, 3.14, 0
     time.sleep(0.2)
     objectCount += 1
 
@@ -152,14 +168,11 @@ def CylinderToHome():
     rob2.send_program(rq_open())
     time.sleep(0.6)
     move(rob2, placeObjectHome_r2, True)
-    gamma += 0.075
+    gamma += 0.071
     #update value
-    placeObjectHome_r2 = list(placeObjectHome_r2)
-    placeObjectHome_r2[1] -= gamma
-    placeObjectHome_r2 = tuple(placeObjectHome_r2)
-    placeObjectHomeDown_r2 = list(placeObjectHomeDown_r2)
-    placeObjectHomeDown_r2[1] -= gamma
-    placeObjectHomeDown_r2 = tuple(placeObjectHomeDown_r2)
+    placeObjectHome_r2 = modify_object_coordinates(placeObjectHome_r2, 1, gamma)
+    placeObjectHomeDown_r2 = modify_object_coordinates(placeObjectHomeDown_r2, 1, gamma)
+    gamma = 0
     time.sleep(0.2)
     objectCount += 1
 
@@ -190,7 +203,9 @@ time.sleep(0.1)
 rob2.set_tcp((0,0,0.16,0,0,0))
 
 move(rob, clearCamera, True)
-move(rob2, clearCamera_2, True)
+move(rob2, clearCamera, True)
 
 #CubeToHome()
-CylinderToHome()
+objectCount = 0
+while objectCount < 4:
+    CubeToHome()
